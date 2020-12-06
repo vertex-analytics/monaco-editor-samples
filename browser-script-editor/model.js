68028ce60d33e832443d8e67a800517ed573d0ea
PlGGS <bbor0422@gmail.com>
2020-12-06 02:37:09 -0600

    Updated v9 Updated default text

--- browser-script-editor/model.js
+++ browser-script-editor/model.js
@@ -1,9 +1,30 @@
 const model = monaco.editor.createModel(
-	[
-		"event;",
-		"pEvent;",
-		"v9;",
-	].join('\n'),
+`class MyFeed extends v9.feed {
+	onInit() {
+\t\t
+	}
+
+	onOpen(pMeta) {
+\t\t
+	}
+
+	onRender() {
+\t\t
+	}
+
+	onEvent(pSymbol, pEvent, pRealTime) {
+\t\t
+	}
+
+	onExit () {
+\t\t
+	}
+
+	onShut () {
+\t\t
+	}
+}
+`,
 	'javascript',
 	monaco.Uri.parse('file:///intellisense.js')
 );
